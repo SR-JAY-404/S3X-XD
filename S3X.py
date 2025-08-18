f64676c835d9f014eaeb909a5a342bb40a14a78b
SR-JAY-404 <152384472+SR-JAY-404@users.noreply.github.com>
2025-08-18 10:27:16 +0600

    Update S3X.py

--- S3X.py
+++ S3X.py
@@ -1,20 +1,8 @@
-import os, platform, time, sys
-os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
-os.system("pip install requests && pip install rich")
-def S1(z):
-    for e in z + '\n':
-        sys.stdout.write(e)
-        sys.stdout.flush()
-        time.sleep(0.02)
-os.system("git pull")
-def Run():
-        bit = platform.architecture()[0]
-        if bit == '64bit':
-            from s3x import s3x
-            welcome()
-        elif bit == '32bit':
-            from s3x import s3x
-            welcome()
-        else:
-            exit('\033[1;31m[●] Connection & Network Error ❌')
-Run()
+import os,time,platform
+os.system('git pull')
+os.system('pip uninstall requests -y');os.system('pip install requests')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import s3x
+else:
+    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
