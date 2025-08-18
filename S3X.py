02517fcbc047d5c0d02d468bcb93109418c05204
SR-JAY-404 <152384472+SR-JAY-404@users.noreply.github.com>
2025-08-18 09:31:45 +0600

    Create S3X.py

---
+++ S3X.py
@@ -0,0 +1,20 @@
+import os, platform, time, sys
+os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
+os.system("pip install requests && pip install rich")
+def S1(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.02)
+os.system("git pull")
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            from s3x import welcome
+            welcome()
+        elif bit == '32bit':
+            from s3x import welcome
+            welcome()
+        else:
+            exit('\033[1;31m[●] Connection & Network Error ❌')
+Run()
